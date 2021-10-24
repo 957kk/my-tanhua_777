cd778a068a3d3c32d9dbb511bc65dc4a79a88f6e
yutao0730 <1138824990@qq.com>
2021-10-24 10:51:52 +0800

    实现登录登出功能，判断token是否有效

---
+++ my-tanhua-common/src/main/java/com/tanhua/common/mapper/FourUserMapper.java
@@ -0,0 +1,8 @@
+package com.tanhua.common.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tanhua.common.pojo.VerifyCode;
+
+
+public interface FourUserMapper extends BaseMapper<VerifyCode> {
+}
---
+++ my-tanhua-manager/my-tanhua-manager-sso/src/main/java/sso/vo/VerifyVo.java
@@ -0,0 +1,12 @@
+package sso.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class VerifyVo implements Serializable {
+    private String uid;
+    private String username;
+    private String avatar;
+}
