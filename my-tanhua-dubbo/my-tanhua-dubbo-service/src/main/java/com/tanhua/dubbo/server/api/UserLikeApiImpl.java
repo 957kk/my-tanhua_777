f176c169f588afb385ad38395110284c69ee1487
mxy <2890690880@qq.com>
2021-10-26 18:49:01 +0800

    修改了圈子审核状态码

--- my-tanhua-dubbo/my-tanhua-dubbo-service/src/main/java/com/tanhua/dubbo/server/api/UserLikeApiImpl.java
+++ my-tanhua-dubbo/my-tanhua-dubbo-service/src/main/java/com/tanhua/dubbo/server/api/UserLikeApiImpl.java
@@ -14,7 +14,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.redis.core.RedisTemplate;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
