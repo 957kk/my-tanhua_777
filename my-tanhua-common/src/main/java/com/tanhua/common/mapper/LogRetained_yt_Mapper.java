7138bfc79cb56fdc7949f388f144c63a597ac34b
yutao0730 <1138824990@qq.com>
2021-10-26 09:45:18 +0800

    日志留存—于涛

---
+++ my-tanhua-common/src/main/java/com/tanhua/common/mapper/LogRetained_yt_Mapper.java
@@ -0,0 +1,8 @@
+package com.tanhua.common.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tanhua.common.pojo.LogRetained_yt;
+
+
+public interface LogRetained_yt_Mapper extends BaseMapper<LogRetained_yt> {
+}
